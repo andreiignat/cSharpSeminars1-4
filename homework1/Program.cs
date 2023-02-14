b737fe7f8e704c524ae89c078ee5c223a6d9751f
andrei ignat <>
2023-02-14 22:18:14 +0200

    Задача 2

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max=" + a + "min=" + b);
+}
+else
+{
+    Console.WriteLine("max=" + b + "min=" + a);
+}
--- lesson1.2/Program.cs
+++ lesson1.2/Program.cs
@@ -1,7 +1,10 @@
-﻿Console.WriteLine("Введите число");
-int n = Convert.ToInt32(Console.ReadLine());
+﻿ 
+ Console.WriteLine("Введите число");
+ int n = Convert.ToInt32(Console.ReadLine());
 
-for(int i = -n; i <= n; i++)
-{
-    Console.WriteLine(i);
-}
+ for(int i = -n; i <= n; i++)
+ {
+     Console.WriteLine(i);
+ }
+
+
