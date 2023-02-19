8bf3eb711799c80053f5eb95801c31803be030cb
andrei ignat <>
2023-02-19 15:01:52 +0200

    Seminar2.1

---
+++ seminar2/lesson1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+int number = Random.Shared.Next (10, 100);
+Console.WriteLine(number);
+int LastDigit = number % 10;
+int FirstDigit = number / 10;
+if (LastDigit > FirstDigit)
+{
+    Console.WriteLine ($"max {LastDigit}, min {FirstDigit}");
+}
+else
+{
+   Console.WriteLine ($"max {FirstDigit}, min {LastDigit}");
+}
---
+++ seminar2/lesson2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = Random.Shared.Next (100, 1000);
+Console.WriteLine(number);
+int LastDigit = number % 10;
+int FirstDigit = number / 100;
+int res = FirstDigit * 10 + LastDigit;
+Console.WriteLine(res);
