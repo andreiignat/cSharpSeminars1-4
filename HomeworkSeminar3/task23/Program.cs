cdf505a36496f72612f8c09451798b0bfea65a98
andrei ignat <>
2023-02-22 22:33:39 +0200

    Задача 23 домашняя работа и плюс классная работа

---
+++ HomeworkSeminar3/task23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу которая принимает на вход число N и выдает таблицу кубов.
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= n)
+{
+    double c = Math.Pow(index, 3);
+    index++;
+    Console.WriteLine(c);
+}
