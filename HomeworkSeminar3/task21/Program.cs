f6c704cef9310db842fe26563b35bb6b1d6b4749
andrei ignat <>
2023-02-22 23:05:56 +0200

    homework task 21, 23

---
+++ HomeworkSeminar3/task21/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу которая принимает координаты двух точек и находит расстояние между ними в 3д пространстве
+//  
+
+Console.WriteLine("Введите координату X первой точки");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату y первой точки");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату z первой точки");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату x второй точки");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату y второй точки");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату y второй точки");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double c = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2 -z1, 2) );
+
+Console.WriteLine(c);
---
+++ seminar3/task19.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+
+while (index <= n)
+{
+    double c = Math.Pow(index, 3);
+    index++;
+    Console.WriteLine(c);
+}
