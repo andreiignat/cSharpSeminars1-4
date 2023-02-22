1052eae171446868386fa027a2d9abbff9aa504e
andrei ignat <>
2023-02-22 20:56:41 +0200

    task18

---
+++ seminar3/task18/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите четверть");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+if (x == 1)
+{
+    Console.WriteLine(" x > 0 и y > 0");
+}
+else if(x == 2)
+{
+    Console.WriteLine("x < 0 && y > 0");
+}
+else if(x == 3)
+{
+    Console.WriteLine("x < 0 && y < 0");
+}
+else if(x == 4)
+{
+    Console.WriteLine("x > 0 && y < 0");
+}
+else
+{
+    Console.WriteLine("невозможно определить координаты");
+}
