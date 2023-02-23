c11347d89035d75929cd8ba553e6ff2b03f62930
andrei ignat <>
2023-02-23 22:54:20 +0200

    task 19

--- HomeworkSeminar3/task19/Program.cs
+++ HomeworkSeminar3/task19/Program.cs
@@ -1,1 +1,19 @@
 ﻿// Напишите программу которая принимает на вход 5ти значное число и проверяет является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void ReadNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число палиндром.");
+  }
+  else 
+    Console.WriteLine($"Ваше число НЕ палиндром.");
+}
+
+if (number!.Length == 5){
+  ReadNumber(number);
+}
+else Console.WriteLine($"Введенное число не пятизначное");
--- seminar3/task19.1/Program.cs
+++ seminar3/task19.1/Program.cs
@@ -2,13 +2,26 @@
 // и выдаёт таблицу квадратов чисел от 1 до N.
 
 
-Console.WriteLine("Введите число");
-int n = Convert.ToInt32(Console.ReadLine());
-int index = 0;
+// Console.WriteLine("Введите число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int index = 0;
 
-while (index <= n)
+// while (index <= n)
+// {
+//     double c = Math.Pow(index, 3);
+//     index++;
+//     Console.WriteLine(c);
+// }
+
+int ReadNumber(string message)
 {
-    double c = Math.Pow(index, 3);
-    index++;
-    Console.WriteLine(c);
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
+
+int N = ReadNumber("Введите число");
+for(int i = 1; i < N; i++)
+{
+    Console.Write(i * i + ", ");
+}
+Console.WriteLine(N * N + ".");
