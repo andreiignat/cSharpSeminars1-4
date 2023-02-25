670438267500ba39f0c7a242b391b091fe18a5c3
andrei ignat <>
2023-02-25 14:55:32 +0200

    Seminar4 task24,26

--- lesson1.2/Program.cs
+++ lesson1.2/Program.cs
@@ -2,7 +2,6 @@
 Console.WriteLine("Введите число");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int otherNumber = 2;
 
 for ( int i = 2; i <= n; i = i+2)
 {      
---
+++ seminar4/task26/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+ int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()); 
+    // int value = Convert.ToInt32(Console.ReadLine());
+    // return value;   2 способа записи.
+}
+
+int CheckLength(int number)
+{ 
+    int count;
+    for(count = 0; number !=0; count++ )
+    {
+        // number /=10; короткая запись  
+        number = number / 10;
+    }
+    return count;
+
+}
+
+ int Usernumber = ReadNumber("введите число");
+ int result = CheckLength(Usernumber);
+ Console.WriteLine(result);
