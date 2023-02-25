ffcb477cc6696168abcea91a2e9c4e55186a8ee3
andrei ignat <>
2023-02-25 11:29:38 +0200

    Записал задачу 19 по новому без строки и массива

--- HomeworkSeminar3/task19/Program.cs
+++ HomeworkSeminar3/task19/Program.cs
@@ -1,20 +1,52 @@
 ﻿// Напишите программу которая принимает на вход 5ти значное число и проверяет является ли оно палиндромом.
 
-Console.Write("Введите пятизначное число: ");
-string? number = Console.ReadLine();
+// Console.Write("Введите пятизначное число: ");
+// string? number = Console.ReadLine();
 
-void ReadNumber(string n)
+// void ReadNumber(string n)
+// {
+//   if (n[0]==n[4] && n[1]==n[3])
+//   {
+//     Console.WriteLine($"Ваше число палиндром.");
+//   }
+//   else 
+//     Console.WriteLine($"Ваше число НЕ палиндром.");
+// }
+
+// if (number!.Length == 5)
+// {
+//   ReadNumber(number);
+// }
+// else Console.WriteLine($"Введенное число не пятизначное");
+
+int ReadNumber(string message)
 {
-  if (n[0]==n[4] && n[1]==n[3])
+  Console.WriteLine(message);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = ReadNumber("Введите пятизначное число");
+int r, t, s = 0;
+t = n;
+
+if (n > 9999 || n > 99999)
   {
-    Console.WriteLine($"Ваше число палиндром.");
-  }
+    while (n > 0)
+    {
+    r = n % 10;
+    s = (s*10)+r;
+    n = n/10;
+    }
+    if (t==s)
+    {
+      Console.WriteLine($"это число {t} палиндром");
+    }
+    else
+    {
+      Console.WriteLine("это число НЕ палиндром");
+    }
+  } 
   else 
-    Console.WriteLine($"Ваше число НЕ палиндром.");
-}
-
-if (number!.Length == 5)
-{
-  ReadNumber(number);
-}
-else Console.WriteLine($"Введенное число не пятизначное");
+  {
+    Console.WriteLine("Введено не верное число");
+  }
