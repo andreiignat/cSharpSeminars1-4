09e962fc9f93f890ae6f797834a3d9ab39305e55
andrei ignat <>
2023-03-03 17:31:03 +0200

    seminar 4

---
+++ seminar4/task30/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+//  заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int number = 8;
+int[] array = new int[number];
+
+for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(0,2);
+        // Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(string.Join(",", array)); // выводит элементы массива
